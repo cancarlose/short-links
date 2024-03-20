1414229ac7330e22454c2567872d2cc12cd76d14
carlos <carlosedlima4@gmail.com>
2024-03-20 14:14:34 -0300

    Add type checking for the request body in the `/links` route.

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import fastify from 'fastify'
-import { z } from 'zod' // ferramenta para validar dados
+import { z } from 'zod'; // ferramenta para validar dados
 
 const app = fastify()
 
 app.post('/links', (request) => {
-
+  const { code, url } = z.object({
+    code: z.string().min(3),
+    url: z.string().url(),
+  }).parse(request.body)
 
   return "Hello World!"
 })
